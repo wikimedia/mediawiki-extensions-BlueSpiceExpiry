c2e9cce225e42a255c911e61f96620ce87a7ca40
pwirth <wirth@hallowelt.biz>
2019-10-21 09:00:53 +0200

    Compatibility to parent setUp method in expiry task tests
    master only
    
    Change-Id: Ic2b3f7db3095026c1fe22e494833a1c7ee02dd28

--- tests/phpunit/ApiExpiryTasksTest.php
+++ tests/phpunit/ApiExpiryTasksTest.php
@@ -14,7 +14,7 @@ class ApiExpiryTasksTest extends BSApiTasksTestBase {
 		return 'bs-expiry-tasks';
 	}
 
-	public function setUp() {
+	public function setUp() : void {
 		parent::setUp();
 		$this->insertPage( 'Dummy' );
 	}
