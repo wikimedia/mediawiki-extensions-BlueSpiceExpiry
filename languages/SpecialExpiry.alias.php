f6cbbd3c560ef2f710e8ed3956a4df82ee5eed15
Umherirrender <umherirrender_de.wp@web.de>
2020-01-10 15:06:38 +0100

    i18n: Restructure alias file
    Alias files are separeted with the language name in all other
    extensions, than it the best to use that format also here
    
    Change-Id: Idddddca174b56e2bb0df9e46eed59329900db02c
    (cherry picked from commit 169852263e0a484e3fbe9e4808ef22bda6603035)

--- languages/SpecialExpiry.alias.php
+++ languages/SpecialExpiry.alias.php
@@ -5,18 +5,21 @@
  * Part of BlueSpice MediaWiki
  *
  * @author     Sebastian Ulbricht <sebastian.ulbricht@dragon-network.hk>
- * @version    $Id: SpecialReview.alias.php 8363 2013-01-30 07:57:11Z swidmann $
  * @package    BlueSpice_Extensions
  * @subpackage Expiry
  * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
  * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
  * @filesource
  */
-$specialPageAliases = [
-	'en' => [
-		'Expiry' => [ 'Expiry', 'Expired_Articles' ]
-	],
-	'de' => [
-		'Expiry' => [ 'Veraltete_Artikel' ]
-	]
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'Expiry' => [ 'Expiry', 'Expired_Articles' ]
+];
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = [
+	'Expiry' => [ 'Veraltete_Artikel' ]
 ];
