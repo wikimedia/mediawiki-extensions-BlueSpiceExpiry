5bd6e1129c880a05c68a95f85807108f463fa073
pwirth <wirth@hallowelt.biz>
2020-01-13 19:15:51 +0100

    Use new hook handler base class for SkinTemplateNavigation
    ERM:10378
    [3.2]
    
    Change-Id: I333786c71c1a0abf4f87ccfeef4dac5b6a470638

--- includes/ExpiryHooks.php
+++ includes/ExpiryHooks.php
@@ -1,38 +1,6 @@
 <?php
 
 class ExpiryHooks {
-	/**
-	 *
-	 * @param SkinTemplate $oSkinTemplate
-	 * @param array &$links
-	 * @return bool
-	 */
-	public static function onSkinTemplateNavigation( $oSkinTemplate, &$links ) {
-		$oUser = RequestContext::getMain()->getUser();
-		$oTitle = RequestContext::getMain()->getTitle();
-
-		if ( !$oUser->isLoggedIn() ) {
-			return true;
-		}
-		if ( $oTitle->exists() === false || $oTitle->isSpecialPage() ) {
-			return true;
-		}
-		if ( !$oUser->isAllowed( 'expirearticle' ) ) {
-			return true;
-		}
-		if ( !$oTitle->userCan( 'read' ) ) {
-			return true;
-		}
-
-		$links['actions']['expiryCreate'] = [
-			"class" => '',
-			"text" => wfMessage( 'bs-expiry-menu-entry-create' )->plain(),
-			"href" => "#",
-			"bs-group" => "hidden"
-		];
-
-		return true;
-	}
 
 	/**
 	 *
