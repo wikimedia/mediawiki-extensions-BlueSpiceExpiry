ce6fa83e58e69ceb343e8072e3d37d9ce29d4aea
pwirth <wirth@hallowelt.biz>
2020-07-07 10:15:35 +0200

    Add "expiration date" and "is expired" SWM properties
    ERM:19981
    [3.1.x]
    
    Change-Id: Iaa99d29626b466dd64ad72365f87b8faf8281a7e

---
+++ src/SMWConnector/PropertyValueProvider/IsExpired.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace BlueSpice\Expiry\SMWConnector\PropertyValueProvider;
+
+use BlueSpice\Expiry\Data\Record;
+use BlueSpice\Expiry\Factory;
+use BlueSpice\SMWConnector\PropertyValueProvider;
+use DateTime;
+use DateTimeZone;
+use MediaWiki\MediaWikiServices;
+use SESP\AppFactory;
+use SMW\DIProperty;
+use SMW\SemanticData;
+use SMWDataItem;
+use SMWDIBoolean;
+
+class IsExpired extends PropertyValueProvider {
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getAliasMessageKey() {
+		return "bs-expiry-smwpropertyvalueprovider-isexpired-alias";
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getDescriptionMessageKey() {
+		return "bs-expiry-smwpropertyvalueprovider-isexpired-desc";
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getType() {
+		return SMWDataItem::TYPE_BOOLEAN;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId() {
+		return '_ISEXPIRED';
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getLabel() {
+		return "Expiry/Is Expired";
+	}
+
+	/**
+	 * @param AppFactory $appFactory
+	 * @param DIProperty $property
+	 * @param SemanticData $semanticData
+	 * @return null
+	 */
+	public function addAnnotation( $appFactory, $property, $semanticData ) {
+		$record = $this->getFactory()->newFromTitle( $semanticData->getSubject()->getTitle() );
+		if ( !$record || !$record->get( Record::DATE, null ) ) {
+			return;
+		}
+		$expires = DateTime::createFromFormat(
+			'YmdHis',
+			$record->get( Record::DATE ),
+			new DateTimeZone( 'UTC' )
+		);
+		$now = new DateTime( 'now', new DateTimeZone( 'UTC' ) );
+		$semanticData->addPropertyObjectValue(
+			$property,
+			new SMWDIBoolean( $expires < $now )
+		);
+	}
+
+	/**
+	 *
+	 * @return Factory
+	 */
+	private function getFactory() {
+		return MediaWikiServices::getInstance()->getService( 'BSExpiryFactory' );
+	}
+}
