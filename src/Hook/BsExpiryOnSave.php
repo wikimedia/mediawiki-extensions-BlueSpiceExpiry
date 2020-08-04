4aa34511568aed956138ea093ed48ee377f8f293
pwirth <wirth@hallowelt.biz>
2020-08-04 13:26:48 +0200

    Add hook handler base classes for ExpiryOnSave and ExpiryOnUpdate
    This also fixes reminders are not updated consistently
    
    ERM:19981
    [3.1.x]
    
    Change-Id: Iedff312d78bd3efac56fcd6fb10d743400abc43c

---
+++ src/Hook/BsExpiryOnSave.php
@@ -0,0 +1,74 @@
+<?php
+namespace BlueSpice\Expiry\Hook;
+
+use BlueSpice\Hook;
+use Config;
+use IContextSource;
+use Title;
+use User;
+
+abstract class BsExpiryOnSave extends Hook {
+	/**
+	 *
+	 * @var \stdClass
+	 */
+	protected $taskData = null;
+
+	/**
+	 *
+	 * @var int
+	 */
+	protected $id = null;
+
+	/**
+	 *
+	 * @var User
+	 */
+	protected $user = null;
+
+	/**
+	 *
+	 * @var Title
+	 */
+	protected $title = null;
+
+	/**
+	 *
+	 * @param \stdClass $taskData
+	 * @param int $id
+	 * @param int $articleId
+	 * @param int $userId
+	 * @param User $user
+	 * @param Title $title
+	 * @return bool
+	 */
+	public static function callback( $taskData, $id, $articleId, $userId, $user, $title ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$taskData,
+			$id,
+			$user,
+			$title
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param \stdClass $taskData
+	 * @param int $id
+	 * @param User $user
+	 * @param Title $title
+	 */
+	public function __construct( $context, $config, $taskData, $id, $user, $title ) {
+		parent::__construct( $context, $config );
+
+		$this->taskData = $taskData;
+		$this->id = $id;
+		$this->user = $user;
+		$this->title = $title;
+	}
+}
---
+++ src/Hook/BsExpiryOnUpdate.php
@@ -0,0 +1,72 @@
+<?php
+namespace BlueSpice\Expiry\Hook;
+
+use BlueSpice\Hook;
+use Config;
+use IContextSource;
+use Title;
+use User;
+
+abstract class BsExpiryOnUpdate extends Hook {
+	/**
+	 *
+	 * @var \stdClass
+	 */
+	protected $taskData = null;
+
+	/**
+	 *
+	 * @var int
+	 */
+	protected $id = null;
+
+	/**
+	 *
+	 * @var User
+	 */
+	protected $user = null;
+
+	/**
+	 *
+	 * @var Title
+	 */
+	protected $title = null;
+
+	/**
+	 *
+	 * @param \stdClass $taskData
+	 * @param int $id
+	 * @param User $user
+	 * @param Title $title
+	 * @return bool
+	 */
+	public static function callback( $taskData, $id, $user, $title ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$taskData,
+			$id,
+			$user,
+			$title
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param \stdClass $taskData
+	 * @param int $id
+	 * @param User $user
+	 * @param Title $title
+	 */
+	public function __construct( $context, $config, $taskData, $id, $user, $title ) {
+		parent::__construct( $context, $config );
+
+		$this->taskData = $taskData;
+		$this->id = $id;
+		$this->user = $user;
+		$this->title = $title;
+	}
+}
