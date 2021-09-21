5025bb0f7b6d32a136e4eac93576beb30640acf9
pwirth <wirth@hallowelt.biz>
2021-09-21 12:49:36 +0200

    Move "expiry create" to secondary actions in discovery skin
    ERM:25778
    [4.x]
    
    Change-Id: Ic6bccca5c5b5539e119ee02bff672a2c176fbd1b

---
+++ src/HookHandler/DiscoverySkin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BlueSpice\Expiry\HookHandler;
+
+use BlueSpice\Discovery\Hook\BlueSpiceDiscoveryTemplateDataProviderAfterInit;
+use BlueSpice\Discovery\ITemplateDataProvider;
+use BlueSpice\Expiry\GlobalActionsTool;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+
+class DiscoverySkin implements
+	MWStakeCommonUIRegisterSkinSlotComponents,
+	BlueSpiceDiscoveryTemplateDataProviderAfterInit
+{
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		$registry->register(
+			'GlobalActionsTools',
+			[
+				'special-bluespice-expiry' => [
+					'factory' => function () {
+						return new GlobalActionsTool();
+					}
+				]
+			]
+		);
+	}
+
+	/**
+	 *
+	 * @param ITemplateDataProvider $registry
+	 * @return void
+	 */
+	public function onBlueSpiceDiscoveryTemplateDataProviderAfterInit( $registry ): void {
+		$registry->unregister( 'toolbox', 'ca-expiryCreate' );
+		$registry->register( 'actions_secondary', 'ca-expiryCreate' );
+	}
+}
