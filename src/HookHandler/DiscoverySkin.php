2f55fdd552e4014146f88d4ba4bc65961f1eaaaf
pwirth <wirth@hallowelt.biz>
2021-10-18 13:38:47 +0200

    Seperate userinterface hooks and fix naming
    ERM:23871
    [4.x]
    
    Depends-On: I30feb0b4abeb3b39e71c14855380e12e19faff96
    Change-Id: I375be9c3a55361ac7e5397eb620a534dc49a9ca0

--- src/HookHandler/DiscoverySkin.php
+++ src/HookHandler/DiscoverySkin.php
@@ -4,29 +4,8 @@ namespace BlueSpice\Expiry\HookHandler;
 
 use BlueSpice\Discovery\Hook\BlueSpiceDiscoveryTemplateDataProviderAfterInit;
 use BlueSpice\Discovery\ITemplateDataProvider;
-use BlueSpice\Expiry\GlobalActionsTool;
-use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
 
-class DiscoverySkin implements
-	MWStakeCommonUIRegisterSkinSlotComponents,
-	BlueSpiceDiscoveryTemplateDataProviderAfterInit
-{
-
-	/**
-	 * @inheritDoc
-	 */
-	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
-		$registry->register(
-			'GlobalActionsTools',
-			[
-				'special-bluespice-expiry' => [
-					'factory' => static function () {
-						return new GlobalActionsTool();
-					}
-				]
-			]
-		);
-	}
+class DiscoverySkin implements BlueSpiceDiscoveryTemplateDataProviderAfterInit {
 
 	/**
 	 *
