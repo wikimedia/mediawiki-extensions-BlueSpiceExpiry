68f8915b82eb668874433b774ca4ddbb3ca582ea
hslater <hamish.slater@hallowelt.com>
2023-12-13 01:48:48 +1300

    Remove legacy 'flyout' implementation
    ERM32026
    
    [REL1_39, REL1_39-4.4.x, master]
    
    Change-Id: I7384d8e79b92500071d312c2f3e7b59a2d3139b2
    (cherry picked from commit afa2ae973f2d236e675cea80a8d3edbb7b6149db)

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -6,21 +6,5 @@ class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 
 	protected function doProcess() {
 		$this->out->addModules( 'ext.bluespice.Expiry' );
-
-		$title = $this->getContext()->getTitle();
-		$canExpire = false;
-		$canDeleteExpiration = false;
-		if ( $title && $title->exists() ) {
-			$canExpire = \MediaWiki\MediaWikiServices::getInstance()
-				->getPermissionManager()
-				->userCan( 'expirearticle', $this->getContext()->getUser(), $title );
-			$canDeleteExpiration = \MediaWiki\MediaWikiServices::getInstance()
-				->getPermissionManager()
-				->userCan( 'expiry-delete', $this->getContext()->getUser(), $title );
-		}
-
-		$this->out->addJsConfigVars( 'bsgExpiryCanExpire', $canExpire );
-		$this->out->addJsConfigVars( 'bsgExpiryCanDeleteExpiration', $canDeleteExpiration );
 	}
-
 }
