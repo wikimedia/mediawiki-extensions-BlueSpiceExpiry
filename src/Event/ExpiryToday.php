865952a29a2467aaa12fa8eaa8b6c6c5db278079
d.savuljesku <d.savuljesku@gmail.com>
2024-04-11 15:26:25 +0200

    Switch to new notifications
    [4.x]
    
    ERM30742
    
    Change-Id: I8c614073b1191a22ec1e4bf8ca324781a887655b

---
+++ src/Event/ExpiryToday.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace BlueSpice\Expiry\Event;
+
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Page\PageIdentity;
+use MediaWiki\User\UserIdentity;
+use Message;
+use MWStake\MediaWiki\Component\Events\BotAgent;
+use MWStake\MediaWiki\Component\Events\Delivery\IChannel;
+use MWStake\MediaWiki\Component\Events\TitleEvent;
+
+class ExpiryToday extends TitleEvent {
+
+	/** @var string */
+	protected $comment;
+
+	/**
+	 * @param PageIdentity $title
+	 * @param string $comment
+	 */
+	public function __construct( PageIdentity $title, string $comment ) {
+		parent::__construct( new BotAgent(), $title );
+		$this->comment = $comment;
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getKeyMessage(): Message {
+		return Message::newFromKey( 'bs-expiry-event-today-desc' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getMessage( IChannel $forChannel ): Message {
+		$msgKey = $this->getMessageKey();
+		if ( $this->comment ) {
+			return Message::newFromKey( $msgKey . '-with-comment' )->params(
+				$this->getTitleAnchor( $this->getTitle(), $forChannel ),
+				$this->comment
+			);
+		}
+		return Message::newFromKey( $msgKey )->params( $this->getTitleAnchor( $this->getTitle(), $forChannel ) );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getMessageKey(): string {
+		return 'bs-expiry-event-today';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getLinksIntroMessage( IChannel $forChannel ): ?Message {
+		return Message::newFromKey( 'ext-notifications-notification-generic-links-intro' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getKey(): string {
+		return 'bs-expiry-today';
+	}
+
+	/**
+	 * @param UserIdentity $agent
+	 * @param MediaWikiServices $services
+	 * @param array $extra
+	 * @return array
+	 */
+	public static function getArgsForTesting(
+		UserIdentity $agent, MediaWikiServices $services, array $extra = []
+	): array {
+		return [
+			$extra['title'] ?? $services->getTitleFactory()->newMainPage(),
+			'dummy comment'
+		];
+	}
+}
