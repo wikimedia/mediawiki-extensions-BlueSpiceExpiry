0e0fa35458d38c14558f12afb973d56eae9fdc9e
hslater <hamish.slater@hallowelt.com>
2024-04-15 14:56:51 +1200

    Convert to abstract schema
    These patches will have been applied before the upgrade to REL1_39,
    and can therefore be removed
    
    ERM36733
    
    [REL1_39, REL1_39-4.4.x, master]
    
    Change-Id: Iaf9f034af7f25ed6f77ddf345a095bda9a55cc6a

--- maintenance/db/bs_expiry.patch.copy_from_reminder_old_version.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM /*$wgDBprefix*/bs_expiry WHERE /*$wgDBprefix*/bs_expiry.expires = 0;
-UPDATE /*$wgDBprefix*/bs_expiry JOIN /*$wgDBprefix*/bs_reminder ON /*$wgDBprefix*/bs_expiry.rem_id = /*$wgDBprefix*/bs_reminder.id
-SET
-    /*$wgDBprefix*/bs_expiry.exp_date = /*$wgDBprefix*/bs_reminder.reminder_date,
-    /*$wgDBprefix*/bs_expiry.exp_page_id = /*$wgDBprefix*/bs_reminder.page_id;
