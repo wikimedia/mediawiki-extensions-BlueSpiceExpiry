36db773574494f3b1901b80d244dd81e50cd944e
Daniel Riedmüller <daniel.riedmueller@hallowelt.com>
2024-08-23 09:21:02 +0200

    Fix js date conversion to YYYY-MM-DD
    ERM38357
    
    [4.5.x]
    
    Change-Id: I87ba612e8311bb79de22e90956a1df0e84e997bd
    (cherry picked from commit b9064abefdf7e884cda8673ef85b81707a944c26)
    (cherry picked from commit c3b92e2db9c4620091e322d7fabc73c465f0c408)

--- resources/ui/page/ExpiryPage.js
+++ resources/ui/page/ExpiryPage.js
@@ -137,8 +137,12 @@ bs.expiry.ui.ExpiryPage.prototype.formatDateForInput = function( date ) {
 	if ( !date ) {
 		return '';
 	}
-	// Awesome JS Date handling
-	return date.toISOString().split('T')[0];
+
+	const year = date.getFullYear();
+	const month = String( date.getMonth() + 1 ).padStart( 2, '0' );
+	const day = String( date.getDate() ).padStart( 2, '0' );
+
+	return `${year}-${month}-${day}`;
 };
 
 bs.expiry.ui.ExpiryPage.prototype.dateFromValue = function( value ) {
