dbc031a5ec2fcdee8fd50d6d8bb6b50dfed80650
Mschlindwein <schlindwein@hallowelt.com>
2024-09-25 15:08:41 +0200

    [WCAG] - switch expiry page info to oojs
    ERM38809
    
    [4.5.x]
    
    Change-Id: I007838442edfc943b4f775d93ca6c591664685e2

--- resources/bluespice.expiry.ExpiryInformationPage.js
+++ resources/bluespice.expiry.ExpiryInformationPage.js
@@ -22,32 +22,53 @@
 
 	bs.expiry.info.ExpiryInformationPage.prototype.onInfoPanelSelect = function () {
 		var me = this;
-		if ( me.expiryGrid === null ){
-			mw.loader.using( 'ext.bluespice.extjs' ).done( function () {
-				Ext.onReady( function( ) {
-					me.expiryGrid = Ext.create( 'BS.Expiry.dataview.NextExpiry', {
-						title: false,
-						renderTo: me.$element[0],
-						width: me.$element.width()
-					});
-				}, me );
-				this.specialPageButton = new OO.ui.ButtonWidget( {
+		if ( me.expiryGrid === null ) {
+			bs.api.store.getData( 'expiry' ).done( function ( data ) {
+				var message = me.getExpiryMessage( data.results[0] );
+				me.expiryLabel = new OO.ui.LabelWidget( {
+					label: message
+				} );
+				me.$element.append( me.expiryLabel.$element );
+				if ( data.results[0] && data.results[0].exp_comment.length > 0 ) {
+					me.expiryCommentLabel = new OO.ui.LabelWidget( {
+						label: mw.message(
+							'bs-expiry-info-dialog-comment',
+							data.results[0].exp_comment
+						).plain()
+					} );
+					me.$element.append( me.expiryCommentLabel.$element );
+				}
+				me.specialPageButton = new OO.ui.ButtonWidget( {
 					label: mw.message( 'bs-expiry-info-dialog-button-label' ).plain(),
 					href: mw.util.getUrl( "Special:Expiry" )
 				} );
-				me.$element.append( this.specialPageButton.$element );
-			});
+				var buttonFieldLayout = new OO.ui.FieldLayout( me.specialPageButton );
+				me.$element.append( buttonFieldLayout.$element );
+			} );
 		}
 	}
 
-	bs.expiry.info.ExpiryInformationPage.prototype.getData = function () {
-		var dfd = new $.Deferred();
-		mw.loader.using( 'ext.bluespice.extjs' ).done( function () {
-			Ext.require( 'BS.Expiry.dataview.NextExpiry', function() {
-				dfd.resolve();
-			});
-		});
-		return dfd.promise();
+	bs.expiry.info.ExpiryInformationPage.prototype.getExpiryMessage = function ( expiryData ) {
+		if ( !expiryData ) {
+			return mw.message( 'bs-expiry-info-dialog-expiry-not-set' ).plain();
+		}
+		var daysRemaining = this.getRemainingDays( expiryData );
+		if ( daysRemaining <= 0 ) {
+			return mw.message( 'bs-expiry-info-dialog-expired' ).plain();
+		}
+		return mw.message(
+			'bs-expiry-info-dialog-current',
+			daysRemaining,
+			expiryData.expiry_date
+		).parse();
+	};
+
+	bs.expiry.info.ExpiryInformationPage.prototype.getRemainingDays = function ( expiryData ) {
+		var today = new Date();
+		var remDate = new Date( expiryData.expiry_date );
+		var msTime = remDate.getTime() - today.getTime();
+		var daysRemaining = Math.ceil( msTime / ( 1000 * 60 * 60 * 24 ) );
+		return daysRemaining;
 	};
 
 	registryPageInformation.register( 'expiry_infos', bs.expiry.info.ExpiryInformationPage );
