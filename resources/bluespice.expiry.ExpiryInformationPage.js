bc422a73e9db5ad40fd3e3853b007d8ec865e981
hslater <hamish.slater@hallowelt.com>
2024-10-08 20:12:25 +1300

    Fix Page information Expiry
    ERM38809
    
    [REL1_39, REL1_39-4.5.x, master]
    
    Change-Id: Ic76b79a4afd529ccef633fa0584032c99124d451

--- resources/bluespice.expiry.ExpiryInformationPage.js
+++ resources/bluespice.expiry.ExpiryInformationPage.js
@@ -1,12 +1,12 @@
-(function( mw, $, bs ) {
+( ( mw, bs ) => {
 	bs.util.registerNamespace( 'bs.expiry.info' );
 
 	bs.expiry.info.ExpiryInformationPage = function ExpiryInformationPage( name, config ) {
-		this.expiryGrid = null;
+		this.expiryLabel = null;
 		bs.expiry.info.ExpiryInformationPage.super.call( this, name, config );
 	};
 
-	OO.inheritClass( bs.expiry.info.ExpiryInformationPage, StandardDialogs.ui.BasePage );
+	OO.inheritClass( bs.expiry.info.ExpiryInformationPage, StandardDialogs.ui.BasePage ); // eslint-disable-line no-undef
 
 	bs.expiry.info.ExpiryInformationPage.prototype.setupOutlineItem = function () {
 		bs.expiry.info.ExpiryInformationPage.super.prototype.setupOutlineItem.apply( this, arguments );
@@ -20,57 +20,71 @@
 		return;
 	};
 
-	bs.expiry.info.ExpiryInformationPage.prototype.onInfoPanelSelect = function () {
-		var me = this;
-		if ( me.expiryGrid === null ) {
-			bs.api.store.getData( 'expiry' ).done( function ( data ) {
-				var message = me.getExpiryMessage( data.results[0] );
-				me.expiryLabel = new OO.ui.LabelWidget( {
-					label: message
-				} );
-				me.$element.append( me.expiryLabel.$element );
-				if ( data.results[0] && data.results[0].exp_comment.length > 0 ) {
-					me.expiryCommentLabel = new OO.ui.LabelWidget( {
-						label: mw.message(
-							'bs-expiry-info-dialog-comment',
-							data.results[0].exp_comment
-						).plain()
-					} );
-					me.$element.append( me.expiryCommentLabel.$element );
-				}
-				me.specialPageButton = new OO.ui.ButtonWidget( {
-					label: mw.message( 'bs-expiry-info-dialog-button-label' ).plain(),
-					href: mw.util.getUrl( "Special:Expiry" )
-				} );
-				var buttonFieldLayout = new OO.ui.FieldLayout( me.specialPageButton );
-				me.$element.append( buttonFieldLayout.$element );
-			} );
-		}
-	}
+	bs.expiry.info.ExpiryInformationPage.prototype.onInfoPanelSelect = async function () {
+		if ( !this.expiryLabel ) {
+			let data;
+			let pageData;
+			let message;
 
-	bs.expiry.info.ExpiryInformationPage.prototype.getExpiryMessage = function ( expiryData ) {
-		if ( !expiryData ) {
-			return mw.message( 'bs-expiry-info-dialog-expiry-not-set' ).plain();
+			try {
+				data = await bs.api.store.getData( 'expiry' );
+				pageData = data.results.find( ( item ) => item.page_title === this.pageName );
+				if ( pageData ) {
+					message = this.getExpiryMessage( pageData.expiry_date );
+				} else {
+					message = mw.message( 'bs-expiry-info-dialog-expiry-not-set' ).text();
+				}
+			} catch ( error ) {
+				message = mw.message( 'bs-expiry-info-dialog-expiry-not-set' ).text();
+			}
+
+			this.expiryLabel = new OO.ui.LabelWidget( {
+				label: message
+			} );
+			const expiryDataLayout = new OO.ui.FieldLayout( this.expiryLabel );
+
+			if ( pageData && pageData.exp_comment ) {
+				const expiryCommentLabel = new OO.ui.LabelWidget( {
+					label: mw.message( 'bs-expiry-info-dialog-comment', pageData.exp_comment ).plain()
+				} );
+				expiryDataLayout.$element.append( expiryCommentLabel.$element );
+			}
+
+			this.$element.append( expiryDataLayout.$element );
+
+			const rights = await mw.user.getRights();
+			if ( rights.includes( 'edit' ) ) { // eslint-disable-line no-restricted-syntax
+				const specialPageButton = new OO.ui.ButtonWidget( {
+					label: mw.message( 'bs-expiry-info-dialog-button-label' ).text(),
+					href: mw.util.getUrl( 'Special:Expiry' )
+				} );
+
+				const buttonFieldLayout = new OO.ui.FieldLayout( specialPageButton );
+				this.$element.append( buttonFieldLayout.$element );
+			}
 		}
-		var daysRemaining = this.getRemainingDays( expiryData );
+	};
+
+	bs.expiry.info.ExpiryInformationPage.prototype.getExpiryMessage = function ( expiryDate ) {
+		const daysRemaining = this.getRemainingDays( expiryDate );
 		if ( daysRemaining <= 0 ) {
-			return mw.message( 'bs-expiry-info-dialog-expired' ).plain();
+			return mw.message( 'bs-expiry-info-dialog-expired' ).text();
 		}
 		return mw.message(
 			'bs-expiry-info-dialog-current',
 			daysRemaining,
-			expiryData.expiry_date
+			expiryDate
 		).parse();
 	};
 
-	bs.expiry.info.ExpiryInformationPage.prototype.getRemainingDays = function ( expiryData ) {
-		var today = new Date();
-		var remDate = new Date( expiryData.expiry_date );
-		var msTime = remDate.getTime() - today.getTime();
-		var daysRemaining = Math.ceil( msTime / ( 1000 * 60 * 60 * 24 ) );
+	bs.expiry.info.ExpiryInformationPage.prototype.getRemainingDays = function ( expiryDate ) {
+		const today = new Date();
+		const remDate = new Date( expiryDate );
+		const msTime = remDate.getTime() - today.getTime();
+		const daysRemaining = Math.ceil( msTime / ( 1000 * 60 * 60 * 24 ) );
 		return daysRemaining;
 	};
 
-	registryPageInformation.register( 'expiry_infos', bs.expiry.info.ExpiryInformationPage );
+	registryPageInformation.register( 'expiry_infos', bs.expiry.info.ExpiryInformationPage ); // eslint-disable-line no-undef
 
-})( mediaWiki, jQuery, blueSpice );
+} )( mediaWiki, blueSpice );
