851839296b3f8165ae371076dcffa3fc8945402c
Robert Vogel <vogel@hallowelt.biz>
2024-10-18 10:43:07 +0200

    Revert "Fix role for global actions entry"
    This reverts commit 304d92698ee896e1281bc85d41ff4e5ec8632a18.

--- src/GlobalActionsEditing.php
+++ src/GlobalActionsEditing.php
@@ -9,9 +9,7 @@ use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink
 class GlobalActionsEditing extends RestrictedTextLink {
 
 	public function __construct() {
-		parent::__construct( [
-			'role' => 'menuitem'
-		] );
+		parent::__construct( [] );
 	}
 
 	/**
