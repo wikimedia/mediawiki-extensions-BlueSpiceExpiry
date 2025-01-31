dc92692d76b93ee67c742e32431cbb7b4bd69a65
Daniel Vogel <daniel.vogel@hallowelt.com>
2025-01-31 14:15:45 +0100

    Integrate into PDFCreator
    [5.0]
    
    Change-Id: Ia2e704c03ba749fd363c7b85af2c01048c2a10f6

---
+++ src/Integration/PDFCreator/StyleBlockProvider/WatermarkStyles.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace BlueSpice\Expiry\Integration\PDFCreator\StyleBlockProvider;
+
+use MediaWiki\Config\Config;
+use MediaWiki\Config\ConfigFactory;
+use MediaWiki\Extension\PDFCreator\Interface\IStyleBlocksProvider;
+use MediaWiki\Extension\PDFCreator\Utility\ExportContext;
+use MediaWiki\User\Options\UserOptionsLookup;
+
+class WatermarkStyles implements IStyleBlocksProvider {
+
+	/** @var Config */
+	private $config;
+
+	/** @var UserOptionsLookup */
+	private $userOptionsLookup;
+
+	/**
+	 * @param ConfigFactory $configFactory
+	 * @param UserOptionsLookup $userOptionsLookup
+	 */
+	public function __construct( ConfigFactory $configFactory, UserOptionsLookup $userOptionsLookup ) {
+		$config = $configFactory->makeConfig( 'bsg' );
+		$this->config = $config;
+		$this->userOptionsLookup = $userOptionsLookup;
+	}
+
+	/**
+	 * @param string $module
+	 * @param ExportContext $context
+	 * @return array
+	 */
+	public function execute( string $module, ExportContext $context ): array {
+		if ( !$this->config->get( 'ExpiryEnablePDFWatermark' ) ) {
+			return [];
+		}
+
+		$userLanguageSetting = $this->userOptionsLookup->getOption(
+			$context->getUserIdentity(),
+			'language'
+		);
+		[ $lang ] = explode( '-', $userLanguageSetting );
+
+		$styles = <<<HEREDOC
+.bs-page-content.expired {
+	background-image: url('images/bg-expired-$lang.png');
+	background-repeat: repeat;
+	background-position: top left;
+	background-size: auto 6cm;
+	min-height: 250px;
+}
+HEREDOC;
+		return [
+			'Expiry'  => $styles
+		];
+	}
+}
